10e33eb16e8624cfa2ea92af019f4ed7638503ff
privaelo <privaeloking@gmail.com>
2026-07-15 09:59:56 -0400

    Restructure: repo root is now src/, adibot moved to subdirectory

---
+++ adibot_rl/setup.py
@@ -0,0 +1,25 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'adibot_rl'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'policy_node = adibot_rl.policy_node:main',
+        ],
+    },
+)
